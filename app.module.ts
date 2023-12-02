2e59737244a880a4b74618019cfac644cdcebc28
azao982 <91277649+azao982@users.noreply.github.com>
2023-12-02 11:57:49 +0100

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import{HttpClientModule} from '@angular/common/http'
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListesdescoursComponent } from './listesdescours/listesdescours.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { AjoutComponent } from './ajout/ajout.component';
+import { SuppComponent } from './supp/supp.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { DetailscoursComponent } from './detailscours/detailscours.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModifiercoursComponent } from './modifiercours/modifiercours.component';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ListesdescoursComponent,
+    NavbarComponent,
+    ConnexionComponent,
+    AjoutComponent,
+    SuppComponent,
+    InscriptionComponent,
+    AproposComponent,
+    DetailscoursComponent,
+    ModifiercoursComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
